e39491e52b7f757a53f9ff210dae1f2f0371be55
coreygamache <coreygamache@hotmail.com>
2018-11-08 08:17:30 -0600

    prototype node submitted for testing

--- sd_hardware_interface/src/drive_motor_node.cpp
+++ sd_hardware_interface/src/drive_motor_node.cpp
@@ -1,7 +1,32 @@
 #include <errno.h>
 #include <ros/ros.h>
-#include <sstream>
+#include <sd_msgs/DriveMotors.h>
 #include <wiringPiI2C.h>
+
+//global variables
+unsigned char pwmValues[2] = {0, 0}; //variable for holding motor pwm output values, {left PWM value, right PWM value}
+
+//callback function called to process messages on motor_(num) topic
+void driveMotorsCallback(const sd_msgs::DriveMotors::ConstPtr& msg)
+{
+
+  //verify left motor PWM value is within PWM limits
+  if (msg->left_motor_pwm > 255)
+    pwmValues[0] = 255;
+  else if (msg->left_motor_pwm < 0)
+    pwmValues[0] = 0;
+  else
+    pwmValues[0] = msg->left_motor_pwm;
+
+  //verify right motor PWM value is within PWM limits
+  if (msg->right_motor_pwm > 255)
+    pwmValues[1] = 255;
+  else if (msg->right_motor_pwm < 0)
+    pwmValues[1] = 0;
+  else
+    pwmValues[1] = msg->right_motor_pwm;
+
+}
 
 int main(int argc, char **argv)
 {
@@ -13,61 +38,52 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "drive_motor_node");
   ros::NodeHandle node_private("~");
 
-  //set loop rate in Hz
-  ros::Rate loop_rate(50);
+  //retrieve arduino i2c address from parameter server
+  int i2c_address;
+  if (!node_private.getParam("/arduino/i2c_address", i2c_address))
+  {
+    ROS_ERROR("arduino i2c address not defined in config file: sd_bringup/config/global.yaml");
+    ROS_BREAK();
+  }
 
-  //initialize i2c communication and data values
-  int fd;
-  int result;
-  unsigned char data[2] = {0, 0};
+  //retrieve refresh rate of sensor in hertz from parameter server
+  float refresh_rate;
+  if (!node_private.getParam("/hardware/drive_motor/refresh_rate", refresh_rate))
+  {
+    ROS_ERROR("line sensor refresh rate not defined in config file: sd_hardware_interface/config/hardware_interface.yaml");
+    ROS_BREAK();
+  }
 
   //initialize i2c protocol and verify connection
-  fd = wiringPiI2CSetup(0x04);
-  ROS_INFO("i2c connection result: %d", fd);
+  int fd = wiringPiI2CSetup(i2c_address);
+  int result; //variable for holding i2c read/write result
+
+  //output notification message and error if one occurs
+  if (fd == -1)
+    ROS_INFO("error establishing i2c connection: %d", errno);
+  else
+    ROS_INFO("i2c connection result: %d", fd);
+
+  //create sunscriber to subscribe to drive motor messages message topic with queue size set to 1000
+  ros::Subscriber drive_motor_sub = node_private.subscribe("drive_motors", 1000, driveMotorsCallback);
+
+  //set loop rate in Hz
+  ros::Rate loop_rate(refresh_rate);
 
   while (ros::ok())
   {
 
-    //update data values then check if they are above PWM limit
-    for (int i = 0; i < 2; i++)
-    {
-
-      //iterate data values
-      if (i == 0)
-      {
-        data[i]++;
-      }
-      else
-      {
-        data[i]--;
-      }
-
-      //verify data values are within PWM limits
-      if (data[i] > 255)
-      {
-        data[i] = 0;
-      }
-      else if (data[i] < 0)
-      {
-        data[i] = 255;
-      }
-
-    }
-
-    //notify of message to be sent
-    ROS_INFO("sending message: %d %d", data[0], data[1]);
-
-    //output motor PWM values via i2c protocol
-    result = write(fd, data, 2);
+    //output motor PWM values to arduino via i2c protocol
+    result = write(fd, pwmValues, 2);
 
     //output notification message if error occurs
     if (result == -1)
-    {
-      ROS_INFO("error: %d", errno);
-    }
+      ROS_INFO("error writing to arduino via i2c: %d", errno);
+
+    //process callback function calls
+    ros::spinOnce();
 
     //sleep until next cycle
-    ros::spinOnce();
     loop_rate.sleep();
   }
 
