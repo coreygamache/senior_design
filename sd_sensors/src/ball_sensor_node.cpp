162de0c5714e4ec5156dd342134b3218caf33425
coreygamache <coreygamache@hotmail.com>
2018-11-07 18:46:58 -0600

    corrected parameter paths

--- sd_sensors/src/ball_sensor_node.cpp
+++ sd_sensors/src/ball_sensor_node.cpp
@@ -14,11 +14,19 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "ball_sensor_node");
   ros::NodeHandle node_private("~");
 
-  //get output pin from parameter server
+  //retrieve output pin from parameter server
   int output_pin;
-  if (!node_private.getParam("ball_sensor/output_pin", output_pin))
+  if (!node_private.getParam("/sensor/ball_sensor/output_pin", output_pin))
   {
     ROS_ERROR("ball sensor output pin not defined in config file: sd_sensors/config/sensors.yaml");
+    ROS_BREAK();
+  }
+
+  //retrieve refresh rate of sensor in hertz from parameter server
+  float refresh_rate;
+  if (!node_private.getParam("/sensor/ball_sensor/refresh_rate", refresh_rate))
+  {
+    ROS_ERROR("ball sensor refresh rate not defined in config file: sd_sensors/config/sensors.yaml");
     ROS_BREAK();
   }
 
@@ -31,18 +39,8 @@ int main(int argc, char **argv)
   //set ball sensor frame id
   ball_msg.header.frame_id = "ball_sensor_link";
 
-  //----------------------------------------------------------
-
   //create publisher to publish ball sensor message with buffer size 10, and latch set to false
   ros::Publisher ball_sensor_pub = node_private.advertise<sd_msgs::Ball>("ball_sensor", 10, false);
-
-  //get refresh rate of sensor in hertz from parameter server
-  float refresh_rate;
-  if (!node_private.getParam("ball_sensor/refresh_rate", refresh_rate))
-  {
-    ROS_ERROR("ball sensor refresh rate not defined in config file: sd_sensors/config/sensors.yaml");
-    ROS_BREAK();
-  }
 
   //set refresh rate of ROS loop to defined refresh rate from parameter server
   ros::Rate loop_rate(refresh_rate);
